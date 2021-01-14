bf880c69efea92e166fe7b14f7ae510b0bf17945
rkhanal <rkhanal@uno.edu>
2021-01-13 23:47:54 -0600

    Color Dependency Implemented

--- my-chess-app/src/ChessBoard/legalMoves/getLegalMovesBishop.js
+++ my-chess-app/src/ChessBoard/legalMoves/getLegalMovesBishop.js
@@ -5,23 +5,29 @@ export function getLegalMovesBishop(board, currentPosition, pieceColor){
     var col = currentPosition[1]
 
     // output array that holds all possible moves for a Bishop
-    var legalMoves = getValidDiagnols(board, row+1, col+1, 1, 1, [])
-    legalMoves = getValidDiagnols(board, row+1, col-1, 1, -1, legalMoves)
-    legalMoves = getValidDiagnols(board, row-1, col-1, -1, -1, legalMoves)
-    legalMoves = getValidDiagnols(board, row-1, col+1, -1, 1, legalMoves)
+    var legalMoves = getValidDiagnols(board, row+1, col+1, 1, 1, [], pieceColor)
+    legalMoves = getValidDiagnols(board, row+1, col-1, 1, -1, legalMoves, pieceColor)
+    legalMoves = getValidDiagnols(board, row-1, col-1, -1, -1, legalMoves, pieceColor)
+    legalMoves = getValidDiagnols(board, row-1, col+1, -1, 1, legalMoves, pieceColor)
     return legalMoves
 }
 
-function getValidDiagnols(board, r, c, row_increment, col_increment, legalMoves){
+function getValidDiagnols(board, r, c, row_increment, col_increment, legalMoves, pieceColor){
     
     while (((r > 0) && (r < 9)) && ((c > 0) && (c < 9))){    
-        if (board[r][c] == null){
+
+        var currentColor = "black";
+        if (board[r][c] > 0){
+            currentColor = "white";
+        }
+        if (board[r][c] === 0){
+            legalMoves.push([r, c])
+        }
+        else if((board[r][c] != null) && (currentColor !== pieceColor)){
+            legalMoves.push([r, c])
             break;
         }
-        else if (board[r][c] === 0){
-            legalMoves.push([r, c])
-        }else{
-            legalMoves.push([r, c])
+        else{
             break;
         }
         r = r + row_increment
